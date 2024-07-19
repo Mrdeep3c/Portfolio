35c8761b30f52abcab83e90a03f438e945950ce9
Deepak Kumar Vishwakarma <122310200+Mrdeep3c@users.noreply.github.com>
2024-07-19 17:44:22 +0530

    first commit

---
+++ Portfolio-Youtube-master/src/components/Footer/index.js
@@ -0,0 +1,125 @@
+import styled from 'styled-components';
+import FacebookIcon from '@mui/icons-material/Facebook';
+import TwitterIcon from '@mui/icons-material/Twitter';
+import LinkedInIcon from '@mui/icons-material/LinkedIn';
+import InstagramIcon from '@mui/icons-material/Instagram';
+import { Bio } from '../../data/constants';
+
+const FooterContainer = styled.div`
+  width: 100%;
+  padding: 2rem 0;
+  display: flex;
+  justify-content: center;
+  //background: linear-gradient(100.26deg, rgba(0, 102, 255, 0.05) 42.33%, rgba(150, 0, 225, 0.05) 127.07%);
+`;
+
+
+const FooterWrapper = styled.footer`
+  width: 100%;
+  max-width: 1200px;
+  display: flex;
+  flex-direction: column;
+  gap: 14px;
+  align-items: center;
+  padding: 1rem;
+  color: ${({ theme }) => theme.text_primary};
+`;
+
+const Logo = styled.h1`
+  font-weight: 600;
+  font-size: 20px;
+  color: ${({ theme }) => theme.primary};
+`;
+
+const Nav = styled.nav`
+  width: 100%;
+  max-width: 800px;
+  margin-top: 0.5rem;
+  display: flex;
+  flex-direction: row;
+  gap: 2rem;
+  justify-content: center;
+  @media (max-width: 768px) {
+    flex-wrap: wrap;
+    gap: 1rem;
+    justify-content: center;
+    text-align: center;
+    font-size: 12px;
+  }
+`;
+
+const NavLink = styled.a`
+color: ${({ theme }) => theme.text_primary};
+  text-decoration: none;
+  font-size: 1.2rem;
+  transition: color 0.2s ease-in-out;
+  &:hover {
+    color: ${({ theme }) => theme.primary};
+  }
+  @media (max-width: 768px) {
+    font-size: 1rem;
+  }
+`;
+
+const SocialMediaIcons = styled.div`
+  display: flex;
+  margin-top: 1rem;
+`;
+
+const SocialMediaIcon = styled.a`
+  display: inline-block;
+  margin: 0 1rem;
+  font-size: 1.5rem;
+  color: ${({ theme }) => theme.text_primary};
+  transition: color 0.2s ease-in-out;
+  &:hover {
+    color: ${({ theme }) => theme.primary};
+  }
+`;
+
+const Copyright = styled.p`
+  margin-top: 1.5rem;
+  font-size: 0.9rem;
+  color: ${({ theme }) => theme.soft2};
+  text-align: center;
+`;
+
+function Footer() {
+  return (
+    <FooterContainer>
+      <FooterWrapper>
+        <Logo>Deepak Kumar Vishwakarma</Logo>
+
+        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d7533.949040321207!2d73.13402880000001!3d19.239942199999994!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3be796f457b1b76f%3A0x35df463ca445bd26!2sKalyan%2C%20Maharashtra!5e0!3m2!1sen!2sin!4v1721386782497!5m2!1sen!2sin" 
+          width="100%" 
+          height="450" 
+          style={{border:0}} 
+          allowFullScreen="" 
+          loading="lazy" 
+          referrerpolicy="no-referrer-when-downgrade">
+        </iframe>
+        
+
+        <Nav>
+          <NavLink href="#about">About</NavLink>
+          <NavLink href="#skills">Skills</NavLink>
+          <NavLink href="#experience">Experience</NavLink>
+          <NavLink href="#projects">Projects</NavLink>
+          <NavLink href="#education">Education</NavLink>
+        </Nav>
+        <SocialMediaIcons>
+          <SocialMediaIcon href={Bio.facebook} target="display"><FacebookIcon /></SocialMediaIcon>
+          <SocialMediaIcon href={Bio.twitter} target="display"><TwitterIcon /></SocialMediaIcon>
+          <SocialMediaIcon href={Bio.linkedin} target="display"><LinkedInIcon /></SocialMediaIcon>
+          <SocialMediaIcon href={Bio.insta} target="display"><InstagramIcon /></SocialMediaIcon>
+        </SocialMediaIcons>
+        <Copyright>
+          &copy; 2023 Deepak Kumar Vishwakarma. All rights reserved.
+        </Copyright>
+
+      </FooterWrapper>
+    </FooterContainer>
+  );
+}
+
+export default Footer;
---
+++ Portfolio-Youtube-master/src/data/constants.js
@@ -0,0 +1,373 @@
+export const Bio = {
+  name: "Deepak Kumar Vishwakarma",
+  roles: [
+    "Front-End Developer",
+    "UI/UX Designer",
+    "Graphic Designer",
+    "Programmer",
+  ],
+  description:
+    "I am a motivated and versatile individual, always eager to take on new challenges. With a passion for learning I am dedicated to delivering high-quality results. With a positive attitude and a growth mindset, I am ready to make a meaningful contribution and achieve great things.",
+  github: "https://github.com/Mrdeep3c",
+  resume:
+    "https://drive.google.com/file/d/1L0GjIuLKOTtmpSyCoBoP20dNKIGB5K2g/view?usp=sharing",
+  linkedin: "https://www.linkedin.com/in/deepak-vishwakarma-169956249/",
+  twitter: "https://x.com/mr_deep3c",
+  insta: "https://www.instagram.com/rishav_chanda/",
+  facebook: "https://www.facebook.com/profile.php?id=100009956525310",
+};
+
+export const skills = [
+  {
+    title: "Frontend",
+    skills: [
+      {
+        name: "React Js",
+        image:
+          "data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9Ii0xMS41IC0xMC4yMzE3NCAyMyAyMC40NjM0OCI+CiAgPHRpdGxlPlJlYWN0IExvZ288L3RpdGxlPgogIDxjaXJjbGUgY3g9IjAiIGN5PSIwIiByPSIyLjA1IiBmaWxsPSIjNjFkYWZiIi8+CiAgPGcgc3Ryb2tlPSIjNjFkYWZiIiBzdHJva2Utd2lkdGg9IjEiIGZpbGw9Im5vbmUiPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIi8+CiAgICA8ZWxsaXBzZSByeD0iMTEiIHJ5PSI0LjIiIHRyYW5zZm9ybT0icm90YXRlKDYwKSIvPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIiB0cmFuc2Zvcm09InJvdGF0ZSgxMjApIi8+CiAgPC9nPgo8L3N2Zz4K",
+      },
+      {
+        name: "HTML",
+        image: "https://www.w3.org/html/logo/badge/html5-badge-h-solo.png",
+      },
+      {
+        name: "CSS",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d5/CSS3_logo_and_wordmark.svg/1452px-CSS3_logo_and_wordmark.svg.png",
+      },
+      {
+        name: "JavaScript",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/6/6a/JavaScript-logo.png/800px-JavaScript-logo.png",
+      },
+      {
+        name: "Bootstrap",
+        image:
+          "https://getbootstrap.com/docs/5.3/assets/brand/bootstrap-logo-shadow.png",
+      },
+      {
+        name: "Tailwind",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/d/d5/Tailwind_CSS_Logo.svg"
+      }
+    ],
+  },
+  {
+    title: "UI/UX",
+    skills: [
+      {
+        name: "Figma",
+        image: "https://upload.wikimedia.org/wikipedia/commons/3/33/Figma-logo.svg",
+      },
+      {
+        name: "Adobe XD",
+        image: "https://upload.wikimedia.org/wikipedia/commons/c/c2/Adobe_XD_CC_icon.svg",
+      },
+      {
+        name: "Sketch",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/5/59/Sketch_Logo.svg",
+      },
+      {
+        name: "Dora",
+        image:
+          "https://pbs.twimg.com/profile_images/1578722318708195328/uZbu0xmy_400x400.jpg",
+      }
+    ],
+  },
+  {
+    title: "Graphic Design",
+    skills: [
+      {
+        name: "Adobe Photoshop",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/a/af/Adobe_Photoshop_CC_icon.svg",
+      },
+      {
+        name: "Adobe Illustrator",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/f/fb/Adobe_Illustrator_CC_icon.svg",
+      },
+      {
+        name: "Canva",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/b/bb/Canva_Logo.svg",
+      },
+      {
+        name: "Adobe InDesign",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/f/ff/Adobe_Indesign_CC_logo.png",
+      },
+    ],
+  },
+  {
+    title: "Others",
+    skills: [
+      {
+        name: "Git",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/3/3f/Git_icon.svg",
+      },
+      {
+        name: "GitHub",
+        image:
+          "https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png",
+      },
+      {
+        name: "VS Code",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/9/9a/Visual_Studio_Code_1.35_icon.svg/512px-Visual_Studio_Code_1.35_icon.svg.png?20210804221519",
+      },
+      {
+        name: "C++",
+        image: 
+          "https://upload.wikimedia.org/wikipedia/commons/1/18/ISO_C%2B%2B_Logo.svg"
+      },
+      {
+        name: "MySQL",
+        image: 
+          "https://upload.wikimedia.org/wikipedia/commons/0/0a/MySQL_textlogo.svg"
+      }
+    ],
+  },
+];
+
+export const experiences = [
+  {
+    id: 0,
+    img: "https://cio.eletsonline.com/wp-content/uploads/2021/06/upgrad.jpg",
+    role: "Machine Learning Intern",
+    company: "UpGrade",
+    date: "15th Jan 2024 - 3rd May 2024",
+    desc: "Working on Healthcare Insurance Project, managing the insurance cost, and streamlining the process with Machine Learning and Regression Algorithms.",
+    skills: [
+      "Python",
+      "Streamlit",
+      "Data Analysis",
+      "Machine Learning Algorithms",
+      "Git/Github",
+    ],
+    doc: "https://media.licdn.com/dms/image/D5622AQFaRvXgV3wFqg/feedshare-shrink_800/0/1720705128346?e=1723680000&v=beta&t=vRvL1Qvrd4QH5VqnkM_of_iLAet82eB9kZ6AVmgED88",
+  }
+];
+
+export const education = [
+  {
+    id: 0,
+    img: "https://upload.wikimedia.org/wikipedia/en/3/3a/Lovely_Professional_University_logo.png",
+    school: "Lovely Professional University, Phagwara (Punjab)",
+    date: "Oct 2020 - May 2024",
+    grade: "7.00 CGPA",
+    desc: "I recently graduated with a Bachelor’s degree in Computer Science and Engineering from Lovely Professional University in Phagwara. I achieved a CGPA of 7.00 during my studies. My coursework covered a range of topics, including Data Structures, Algorithms, Object-Oriented Programming, Database Management Systems, Operating Systems, and Computer Networks.",
+    degree: "Bachelor of Technology - BTech, Computer Science and Engineering",
+  },
+  {
+    id: 1,
+    img: "https://i.pinimg.com/736x/5a/4e/f9/5a4ef9b9d7806da10afb1e9b3efdb22d.jpg",
+    school: "Kendriya Vidyalaya, Jayant (Madhya Pradesh)",
+    date: "Apr 2018 - Apr 2019",
+    grade: "78%",
+    desc: "I completed my class 12 high school education at Kendriya Vidyalaya, Jayant, where I studied Science(Math) with Computer Science.",
+    degree: "CBSE, Science with Computer",
+  },
+  {
+    id: 2,
+    img: "https://i.pinimg.com/736x/5a/4e/f9/5a4ef9b9d7806da10afb1e9b3efdb22d.jpg",
+    school: "Kendriya Vidyalaya, Jayant (Madhya Pradesh)",
+    date: "Apr 2016 - Apr 2017",
+    grade: "8.4 CGPA",
+    desc: "I completed my class 10 education at Kendriya Vidyalaya, Jayant, where I studied Science with Computer Application.",
+    degree: "CBSE, Science with Computer",
+  },
+];
+
+export const projects = [
+  {
+    id: 9,
+    title: "Healthcare Insurance Cost",
+    date: "Jan 2024 - May 2024",
+    description:
+      "Medical insurance is one of the most crucial aspects of healthcare, and machine learning techniques can play a significant role in analyzing and predicting medical insurance costs. The goal of this project is to build a predictive model that can estimate the medical insurance costs for individuals based on their age, gender, BMI, smoking status, and other related factors. The dataset used in this project contains information about medical insurance costs for a group of people based on these factors. ",
+    image:
+      "https://media.licdn.com/dms/image/D5622AQEIyn6Ho67aIw/feedshare-shrink_2048_1536/0/1721124257211?e=1724284800&v=beta&t=Qp49Temj5VnWj7AlFTIgt0vdYGwC6te6Fe6wwCQwQrk",
+    tags: [
+      "Machine Learning",
+      "Algorithms",
+      "NumPy",
+      "Pandas",
+      "Matplotlib",
+      "Seaborn",
+      "Streamlit",
+      "Github",
+      "Python",
+    ],
+    category: "machine learning",
+    github: "https://github.com/Mrdeep3c/Healthcare_Prediction",
+    webapp: "https://healthcareprediction-ju2x73mvjf85mgm6aba9kr.streamlit.app/?utm_medium=social",
+    member: [
+      {
+        name: "Rishav Chanda",
+        img: "https://avatars.githubusercontent.com/u/64485885?v=4",
+        linkedin: "https://www.linkedin.com/in/rishav-chanda-b89a791b3/",
+        github: "https://github.com/rishavchanda/",
+      },
+      {
+        name: "Upasana Chaudhuri",
+        img: "https://avatars.githubusercontent.com/u/100614635?v=4",
+        linkedin: "https://www.linkedin.com/in/upasana-chaudhuri-2a2bb5231/",
+        github: "https://github.com/upasana0710",
+      },
+    ],
+  },
+  {
+    id: 0,
+    title: "Image Recognization CIFAR-10",
+    date: "Mar 2023 - May 2023",
+    description:
+      "Developed a full-stack web application that allows users to search for, play, and pause their favorite podcasts on demand and create podcasts. Implemented user authentication using Google Auth and Jwt Auth, made responsive user interface with React JS that provides users with a seamless experience across all devices. Practiced agile methodologies to optimize team efficiency and communication.",
+    image:
+      "https://user-images.githubusercontent.com/64485885/234602896-a1bd8bcc-b72b-4821-83d68ad885bf435e.png",
+    tags: ["Machine Learning",
+      "Algorithms",
+      "NumPy",
+      "Pandas",
+      "Matplotlib",
+      "Seaborn",
+      "Github",
+      "Python",],
+    category: "machine learning",
+    github: "https://github.com/Mrdeep3c/Cifar-10",
+    webapp: "",
+  },
+  {
+    id: 1,
+    title: "RBK Education Institutions Clone",
+    date: "Jan 2023 - Mar 2023",
+    description:
+      "Designed and developed the RBK Education Institutions School website clone.",
+    image:
+      "https://media.licdn.com/dms/image/D5622AQG7m_uanoq4AQ/feedshare-shrink_2048_1536/0/1721125846637?e=1724284800&v=beta&t=DAEk6wytJw4UVZoPKV3yWS8U27JWoCspqALbcm6CiPo",
+    tags: [
+      "HTML",
+      "CSS",
+      "Js",
+      "Tailwind",
+      "Github",
+    ],
+    category: "web app",
+    github: "https://github.com/Mrdeep3c/School-Website",
+    webapp: "https://mrdeep3c.github.io/School-Website/",
+  },
+  {
+    id: 2,
+    title: "Drum-Kit",
+    date: "Jul 2023 - Aug 2023",
+    description:
+      "Drum Kit Website, the ultimate online and offline drum kit experience! Whether you're a seasoned drummer or just starting out, our interactive drum kit allows you to practice and play right from your browser using mouse or keyboard.",
+    image:
+      "https://cdn.dribbble.com/userupload/15624878/file/original-d37e0da0311979e8b24b62ff96b5367a.png?resize=2140x1174",
+    tags: ["HTML", "CSS", "JS", "Github"],
+    category: "web app",
+    github: "https://github.com/Mrdeep3c/Drum-Kits",
+    webapp: "https://mrdeep3c.github.io/Drum-Kits/",
+  },
+  {
+    id: 3,
+    title: "Dice Game",
+    date: "Jun 2023 - Jul 2023",
+    description:
+      "The webpage has been developed using HTML, CSS, and JavaScript to create a dice game for two players, with functionality to determine the winner by simply refreshing the page.",
+    image:
+      "https://cdn.dribbble.com/userupload/15625086/file/original-6d2a7a4e5b61a0d616253e69b2fa4112.png?resize=2158x1177",
+    tags: ["HTML", "CSS", "JS", "Github"],
+    category: "web app",
+    github: "https://github.com/Mrdeep3c/Dice-Game",
+    webapp: "https://mrdeep3c.github.io/Dice-Game/",
+  },
+  {
+    id: 10,
+    title: "Tindog",
+    date: "Jun 2023 - Jul 2023",
+    description:
+      "Tindog is a clone of the Tinder website, created using HTML, CSS, and Bootstrap. This project showcases my ability to replicate complex web applications, featuring a responsive design and user-friendly interface. It demonstrates my skills in front-end development and my proficiency with modern web development technologies.",
+    image:
+      "https://cdn.dribbble.com/userupload/15625299/file/original-1315fd6b45801c214c745a6e086bf2c9.png?resize=1435x955",
+    tags: ["HTML", "CSS", "JS", "Bootstrap", "Tailwind", "Github"],
+    category: "web app",
+    github: "https://github.com/Mrdeep3c/Tindog",
+    webapp: "https://mrdeep3c.github.io/Tindog/",
+  },
+  {
+    id: 4,
+    title: "Move it",
+    date: "May 2023 - Jun 2023",
+    description:
+      "Move It is a website developed using HTML, CSS, and Bootstrap, dedicated to providing exceptional moving services to customers across the US. As a startup, our mission is to simplify and enhance the moving experience, ensuring it is stress-free and straightforward. We are passionate about making the process as seamless and enjoyable as possible, reflecting our commitment to customer satisfaction and innovation in the moving industry.",
+    image:
+      "https://cdn.dribbble.com/userupload/15625433/file/original-b1be6dda4aed1849300e5d8aa5955daa.png?resize=2158x1173",
+    tags: ["HTML", "CSS", "JS", "Bootstrap", "Tailwind", "Github"],
+    category: "web app",
+    github: "https://github.com/Mrdeep3c/Move-It",
+    webapp: "https://mrdeep3c.github.io/Move-It/",
+  },
+  {
+    id: 5,
+    title: "Nike Air Max",
+    date: "Jun 2023",
+    description:
+      "I designed a user interface for the NIKE AIR MAX 90 shoe using Figma, incorporating creative ideas and providing comprehensive instructions and details. This project highlights my skills in UI/UX design, showcasing my ability to blend aesthetics with functionality to create an engaging and informative user experience.",
+    image:
+      "https://media.licdn.com/dms/image/C4D22AQFXgRLH7FcI5A/feedshare-shrink_2048_1536/0/1674407182835?e=1724284800&v=beta&t=6jaagfPmCGl7PjDbDjd763EJnm376wpY8sobKc0RjVI",
+    tags: ["UI", "UX", "Figma", "Photoshop"],
+    category: "UI/UX",
+    github: "https://github.com/Mrdeep3c/NIKE-AIRMAX-90",
+    webapp: "https://www.figma.com/design/2ziOweCPq2bfA5ZcvX7Twq/NIKE-AIR-MAX-90?node-id=1-2&t=dBhmPraRSIO7UZey-1",
+  },
+  {
+    id: 6,
+    title: "Gaming Console",
+    date: "Jun 2023",
+    description:
+      "I designed a user interface for the Gaming Console using Figma, incorporating creative ideas and providing comprehensive instructions and details. This project highlights my skills in UI/UX design, showcasing my ability to blend aesthetics with functionality to create an engaging and informative user experience.",
+    image:
+      "https://cdn.dribbble.com/userupload/15625641/file/original-8814a347096b3d6fbaf4f88414edfd12.png?resize=752x",
+    tags: ["UI", "UX", "Figma", "Photoshop"],
+    category: "UI/UX",
+    github: "https://github.com/Mrdeep3c/Gaming-Console-UI-Design-",
+    webapp: "https://www.figma.com/design/BEmSdyP8dUK1vz4PHYpBGv/Console?node-id=0-1&t=9BHYQDscWs6quhE0-1",
+  },
+  {
+    id: 7,
+    title: "SignIn/SignUp Page",
+    date: "May 2023",
+    description:
+      "I designed a user interface for the SignIn/SignUp Page using Figma, incorporating creative ideas and providing comprehensive instructions and details. This project highlights my skills in UI/UX design, showcasing my ability to blend aesthetics with functionality to create an engaging and informative user experience.",
+    image:
+      "https://cdn.dribbble.com/userupload/8541773/file/original-b4c2dd5da53ce371f51a370d7274407f.jpg?resize=1504x1039",
+    tags: ["UI", "UX", "Figma", "Prototype", "Photoshop"],
+    category: "UI/UX",
+    github: "",
+    webapp: "https://www.figma.com/design/dADRdxATcvBBOCafVZCUEN/Log-In?node-id=0-1&t=0AjnVPcIBbf8mEed-1",
+  },
+  {
+    id: 8,
+    title: "FinStreet Treding Website",
+    date: "Jan 2021",
+    description:
+      "I designed a user interface for the FinStreet into modern style clone using Figma, incorporating creative ideas and providing comprehensive instructions and details. This project highlights my skills in UI/UX design, showcasing my ability to blend aesthetics with functionality to create an engaging and informative user experience.",
+    image:
+      "https://cdn.dribbble.com/userupload/15625830/file/original-bed51853e57b1417d2925d2d0426dff5.png?resize=1504x862",
+    tags: ["UI", "UX", "Figma", "Prototype", "Photoshop"],
+    category: "UI/UX",
+    github: "https://www.figma.com/design/pVEgVK8IvOqVlD5ZojEimx/FinStreet?node-id=0-1&t=3LuwrQQU66a3kzWK-1",
+    webapp: "https://www.figma.com/proto/pVEgVK8IvOqVlD5ZojEimx/FinStreet?page-id=0%3A1&node-id=2-2&viewport=427%2C24%2C0.12&t=l2gsZKUXzbdk26cf-1&scaling=min-zoom&content-scaling=fixed",
+  },
+];
+
+export const TimeLineData = [
+  { year: 2017, text: "Started my journey" },
+  { year: 2018, text: "Worked as a freelance developer" },
+  { year: 2019, text: "Founded JavaScript Mastery" },
+  { year: 2020, text: "Shared my projects with the world" },
+  { year: 2021, text: "Started my own platform" },
+];
